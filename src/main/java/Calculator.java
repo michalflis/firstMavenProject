75a699704ff6efe4e27438c72418f77ba1f6f81d
michalflis <michalt.flis@gmail.com>
2021-06-24 22:06:28 +0200

    Added calculator with single tests

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return (a / (double) b);
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,75 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void ShouldAddTwoValues_11And17() {
+        // given
+        Calculator calculator = new Calculator();
+        int a = 11;
+        int b = 17;
+
+        // when
+        int result = calculator.sum(a, b);
+
+        // then
+        Assert.assertEquals(28, result);
+    }
+
+    @Test
+    public void ShouldAddSubtractTwoValues_10And3() {
+        // given
+        Calculator calculator = new Calculator();
+        int a = 10;
+        int b = 3;
+
+        // when
+        int result = calculator.subtract(a, b);
+
+        // then
+        Assert.assertEquals(7, result);
+    }
+
+    @Test
+    public void ShouldAddMultiplyTwoValues_2And66() {
+        // given
+        Calculator calculator = new Calculator();
+        int a = 2;
+        int b = 66;
+
+        // when
+        int result = calculator.multiply(a, b);
+
+        // then
+        Assert.assertEquals(132, result);
+    }
+
+    @Test
+    public void ShouldAddDivideTwoValues_0And7() {
+        // given
+        Calculator calculator = new Calculator();
+        int a = 0;
+        int b = 7;
+
+        // when
+        double result = calculator.divide(a, b);
+
+        // then
+        Assert.assertEquals(0, result,0.0001);
+    }
+
+    @Test
+    public void ShouldAddDivideTwoValues_11And3() {
+        // given
+        Calculator calculator = new Calculator();
+        int a = 11;
+        int b = 3;
+
+        // when
+        double result = calculator.divide(a, b);
+
+        // then
+        Assert.assertEquals(3.6666, result,0.0001);
+    }
+}

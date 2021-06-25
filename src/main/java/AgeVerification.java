62e0714a4cdd201601bba0e46eed2b1050aab904
michalflis <michalt.flis@gmail.com>
2021-06-25 19:31:35 +0200

    Optimized AgeVerification method

--- src/main/java/AgeVerification.java
+++ src/main/java/AgeVerification.java
@@ -1,11 +1,8 @@
 public class AgeVerification {
 
     public static boolean isAdult(int a) {
-        if (a>=18) {
-            return true;
-        } else {
-            return false;
-        }
+        return a>=18;
+
     }
 
 }

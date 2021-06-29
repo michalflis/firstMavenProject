f259b8aa5159460b1b29c8c2f7505f603afa6036
michalflis <michalt.flis@gmail.com>
2021-06-29 21:42:52 +0200

    Created movie class

---
+++ src/main/java/Movie.java
@@ -0,0 +1,9 @@
+public class Movie {
+
+    private String title;
+    private String directorName;
+    private String directorSurname;
+    private int lenghtInMinutes;
+    private int yearOfProduction;
+
+}

2c51feaaff2991a771a26ab39f160621b1385a3e
michalflis <michalt.flis@gmail.com>
2021-06-30 17:43:24 +0200

    Added constructor to Movie class

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -6,4 +6,12 @@ public class Movie {
     private int lenghtInMinutes;
     private int yearOfProduction;
 
+    public Movie(String title, String directorName, String directorSurname, int lenghtInMinutes, int yearOfProduction) {
+        this.title = title;
+        this.directorName = directorName;
+        this.directorSurname = directorSurname;
+        this.lenghtInMinutes = lenghtInMinutes;
+        this.yearOfProduction = yearOfProduction;
+    }
+
 }

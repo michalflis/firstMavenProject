798598e0f314f47ffbf7bcb298f3d17abc30d595
michalflis <michalt.flis@gmail.com>
2021-07-01 17:16:18 +0200

    Corrected class hermetization

--- src/main/java/CinemaSeat.java
+++ src/main/java/CinemaSeat.java
@@ -1,12 +1,20 @@
 public class CinemaSeat {
 
-    private int seatNumber;
-    private int rowNumber;
-    boolean isTaken;
+    private final int SEAT_NUMBER;
+    private final int ROW_NUMBER;
+    private boolean isTaken;
 
     public CinemaSeat(int seatNumber, int rowNumber, boolean isTaken) {
-        this.seatNumber = seatNumber;
-        this.rowNumber = rowNumber;
+        SEAT_NUMBER = seatNumber;
+        ROW_NUMBER = rowNumber;
+        this.isTaken = isTaken;
+    }
+
+    public boolean getIsTaken() {
+        return isTaken;
+    }
+
+    public void setIsTaken(boolean isTaken) {
         this.isTaken = isTaken;
     }
 

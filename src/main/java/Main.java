f9f76890ad622104ab4f58d150c777fcea9835fe
michalflis <michalt.flis@gmail.com>
2021-07-07 17:23:15 +0200

    Simpliefied the code

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        WeekdayName weekdayName = new WeekdayName();
+
+        System.out.println(weekdayName.getWeekdayName(Weekday.Tuesday));
+    }
+}
--- src/main/java/WeekdayName.java
+++ src/main/java/WeekdayName.java
@@ -2,19 +2,7 @@ public class WeekdayName {
 
     public String getWeekdayName (Weekday weekday) {
 
-        switch (weekday) {
-            case Monday : return "Monday";
-            case Tuesday : return "Tuesday";
-            case Wednesday : return "Wednesday";
-            case Thursday : return "Thursday";
-            case Friday : return "Friday";
-            case Saturday : return "Saturday";
-            case Sunday : return "Sunday";
-
-        }
-
-        return null;
-
+        return weekday.toString();
     }
 
 }
